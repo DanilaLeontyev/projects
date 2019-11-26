600cb5ada5edce92fc4b745f8ec7b5355eb6fe93
Danila Leontyev <danila.leontyev.web@gmail.com>
2019-11-26 19:14:30 +0500

    inti shri20

---
+++ shri20/task2/linter.js
@@ -0,0 +1,32 @@
+const json = `{
+  "block": "warning",
+  "content": [
+        {
+          "block": "placeholder",
+          "mods": { "size": "m" }
+        },
+        {
+          "elem": "content",
+          "content": [
+              {
+                "block": "text",
+                "mods": { "size": "m" }
+              },
+              {
+                  "block": "text",
+                  "mods": { "size": "l" }
+                }
+          ]
+        }
+  ]
+}`;
+
+const rule = {
+  
+}
+
+function lint([arrFunc], obj) {
+
+}
+
+lint(json);
